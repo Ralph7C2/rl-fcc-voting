5d08be6b8d04245ecf70cbfff7235bee2920623a
ralph7c2 <ralph7c2@gmail.com>
2017-01-21 20:27:45 -0600

    Added passport

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const dotenv = require('dotenv').load();
+require('dotenv').load();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const githubStrategy = require('passport-github').Strategy;
+passport.use(new githubStrategy({
+	clientID: process.env.GITHUB_CLIENT_ID,
+	clientSecret: process.env.GITHUB_CLIENT_SECRET,
+	callbackURL: process.env.GITHUB_CLIENT_CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+	User.findOrCreate({gihubId: profile.id }, function(err, user) {
+		return cb(err, user)
+	});
+}));
+
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send("Hello world!");
-});
+app.get('/', express.static('public'));
+
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login'}),
+	function(req, res) {
+		res.redirect('/');
+	}
+);
 
 var port = process.env.PORT || 4040;
 app.listen(port, function() {
